e16fdaa1116edc0b7676ab629c47d29201897116
Abhishek SIngh <71981463+Abh1shekSingh@users.noreply.github.com>
2022-05-19 08:23:36 +0530

    24hVolume data fixed

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -18,13 +18,15 @@ const CryptoDetails = () => {
     console.log(coinHistory);
     if( isFetching ) return <Loader />;
     const cryptoDetails = data?.data?.coin ;
+    console.log(cryptoDetails)
 
+    
     //const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
     const stats = [
         { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
         { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
-        { title: '24h Volume', value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
+        { title: '24h Volume', value: `$ ${cryptoDetails?.['24hVolume'] && millify(cryptoDetails?.['24hVolume'])}`, icon: <ThunderboltOutlined /> },
         { title: 'Market Cap', value: `$ ${cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
         { title: 'All-time-high(daily avg.)', value: `$ ${cryptoDetails?.allTimeHigh?.price && millify(cryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
       ];
